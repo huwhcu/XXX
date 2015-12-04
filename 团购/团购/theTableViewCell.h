3409f1654d4b37343b1df7c79143b3fec919ed36
liuchongwen <huwhcu@126.com>
2015-12-04 22:19:00 +0800

    完成

---
+++ 团购/团购/theTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  theTableViewCell.h
+//  团购
+//
+//  Created by qingyun on 15/12/4.
+//  Copyright (c) 2015年 QingYun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface theTableViewCell : UITableViewCell
+
+@end
---
+++ 团购/团购/theTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  theTableViewCell.m
+//  团购
+//
+//  Created by qingyun on 15/12/4.
+//  Copyright (c) 2015年 QingYun. All rights reserved.
+//
+
+#import "theTableViewCell.h"
+
+@implementation theTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
