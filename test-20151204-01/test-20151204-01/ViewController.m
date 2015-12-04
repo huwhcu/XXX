df047ed0ea654bc82c16ae6d4ff9691e029e9c70
liuchongwen <huwhcu@126.com>
2015-12-04 10:32:19 +0800

    完成基本索引练习

--- test-20151204-01/test-20151204-01/ViewController.m
+++ test-20151204-01/test-20151204-01/ViewController.m
@@ -8,15 +8,83 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic, strong) NSDictionary *dict;
+@property (nonatomic, strong) NSArray *keys;
 
 @end
 
 @implementation ViewController
 
+- (void)loddDictFromFile
+{
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"sortednames" ofType:@"plist"];
+    _dict = [NSDictionary dictionaryWithContentsOfFile:path];
+    
+    NSArray *keys = _dict.allKeys;
+    
+    _keys = [keys sortedArrayUsingSelector:@selector(compare:)];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self loddDictFromFile];
+    
+    UITableView *tableView = [[UITableView alloc] initWithFrame:[UIScreen mainScreen].applicationFrame style:UITableViewStylePlain];
+    [self.view addSubview:tableView];
+    
+    tableView.dataSource = self;
+    tableView.delegate = self;
+    
+//  行数超过时，显示右侧索引
+    tableView.sectionIndexMinimumDisplayRowCount = 1000;
+    tableView.sectionIndexColor = [UIColor grayColor];
+    tableView.backgroundColor = [UIColor greenColor];
+    tableView.sectionIndexTrackingBackgroundColor = [UIColor whiteColor];
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return _keys.count;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    NSString *key = _keys[section];
+    NSArray *array = _dict[key];
+    return array.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *identifier = @"cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+    }
+    NSString *key = _keys[indexPath.section];
+    NSArray *array = _dict[key];
+    cell.textLabel.text = array[indexPath.row];
+    return cell;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    return _keys[section];
+}
+
+- (NSArray *)sectionIndexTitlesForTableView:(UITableView *)tableView
+{
+    return _keys;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView sectionForSectionIndexTitle:(NSString *)title atIndex:(NSInteger)index
+{
+    return index;
 }
 
 - (void)didReceiveMemoryWarning {
