0ed0d5d2f94c9ecb719431b5f38d53607700976c
liuchongwen <huwhcu@126.com>
2015-12-05 17:09:07 +0800

    创建section折叠练习项目

---
+++ LOL练习/LOL练习/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LOL练习
+//
+//  Created by qingyun on 15/12/5.
+//  Copyright (c) 2015年 QingYun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LOL练习/LOL练习Tests/LOL__Tests.m
@@ -0,0 +1,40 @@
+//
+//  LOL__Tests.m
+//  LOL练习Tests
+//
+//  Created by qingyun on 15/12/5.
+//  Copyright (c) 2015年 QingYun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface LOL__Tests : XCTestCase
+
+@end
+
+@implementation LOL__Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
