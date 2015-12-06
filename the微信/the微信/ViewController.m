8aea28f6d3afe4683357bc2782f402dd9326f56a
liuchongwen <huwhcu@126.com>
2015-12-06 14:08:09 +0800

    创建微信的练习项目

---
+++ the微信/the微信/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  the微信
+//
+//  Created by qingyun on 15/12/6.
+//  Copyright (c) 2015年 QingYun. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ the微信/the微信/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  the微信
+//
+//  Created by qingyun on 15/12/6.
+//  Copyright (c) 2015年 QingYun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
