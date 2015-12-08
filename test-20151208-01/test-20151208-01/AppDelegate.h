4e88f36ac22cf651287bffb1dc7c613093453ab0
liuchongwen <huwhcu@126.com>
2015-12-08 17:01:23 +0800

    创建1208的练习项目

---
+++ test-20151208-01/test-20151208-01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test-20151208-01
+//
+//  Created by qingyun on 15/12/8.
+//  Copyright (c) 2015年 QingYun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test-20151208-01/test-20151208-01/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test-20151208-01
+//
+//  Created by qingyun on 15/12/8.
+//  Copyright (c) 2015年 QingYun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
