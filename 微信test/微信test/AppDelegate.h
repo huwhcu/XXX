7dd2d388a6994402405a04b8e5723c13bbd31611
liuchongwen <huwhcu@126.com>
2015-12-09 21:00:51 +0800

    重新创建微信练习项目

---
+++ 微信test/微信test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  微信test
+//
+//  Created by qingyun on 15/12/9.
+//  Copyright (c) 2015年 QingYun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 微信test/微信test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  微信test
+//
+//  Created by qingyun on 15/12/9.
+//  Copyright (c) 2015年 QingYun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
