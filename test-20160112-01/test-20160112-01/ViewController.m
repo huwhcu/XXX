26bf9bb10cb0ab85edc160d1ade8211745c04a24
liuchongwen <huwhcu@126.com>
2016-01-12 19:23:58 +0800

    久违的更新……

--- test-20160112-01/test-20160112-01/ViewController.m
+++ test-20160112-01/test-20160112-01/ViewController.m
@@ -8,7 +8,12 @@
 
 #import "ViewController.h"
 
+#import <AFNetworking.h>
+#import <Masonry.h>
+
 @interface ViewController ()
+
+@property (nonatomic, strong) UIView *theView1;
 
 @end
 
@@ -17,6 +22,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor grayColor];
+    _theView1 = [[UIView alloc] init];
+    _theView1.backgroundColor= [UIColor redColor];
+    [self.view addSubview:_theView1];
+    
+    UIEdgeInsets padding = UIEdgeInsetsMake(20, 20, 20, 20);
+    
+    [_theView1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.view.mas_top).with.offset(padding.top);
+        make.left.equalTo(self.view.mas_left).with.offset(padding.left);
+        make.bottom.equalTo(self.view.mas_bottom).with.offset(-padding.bottom);
+        make.right.equalTo(self.view.mas_right).with.offset(-padding.right);
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
